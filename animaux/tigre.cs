82dbaf77443af53993a2c7396f72e3786b7c0096
Louka <loukapc1@gmail.com>
2026-03-24 13:55:32 +0100

    refactor: suprimer la class aigle du fichier

--- animaux/tigre.cs
+++ animaux/tigre.cs
@@ -13,13 +13,3 @@ public class Tigre : Animal
         return rand.Next(1, 4); 
     }
 }
-
-public class Aigle : Animal
-{
-    public override int EsperanceVieMois => 168;
-    public override int MaturiteSexuelleMois => 48;
-    public override int DureeGestationMois => 2; 
-    public override double ConsommationQuotidienneKg => 0.25;
-
-    public Aigle(string genre, int ageMois) : base(genre, ageMois) { }
-}
